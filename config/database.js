c0c3c972f681d850a3668d93f16b4c31be37ab1a
Chris Holston <chrisholston@me.com>
2020-11-10 18:49:37 -0600

    added database.js

---
+++ config/database.js
@@ -0,0 +1,16 @@
+// require our modules 
+const mongoose = require ('mongoose');
+
+// create a shortcut variable
+const db = mongoose.connection;
+
+// connect to the database
+mongoose.connect('mongodb+srv://admin:1234@cluster0.wyypc.mongodb.net/mongoose-flights?retryWrites=true&w=majority')
+useNewUrlParser: true,
+useCreateIndex: true,
+useUnifiedTopology: true
+});
+// listen for a connection event
+db.on('connected',function() {
+    console.log(`Connected to MongoDB on ${db.host}:${db.port}`)
+});
