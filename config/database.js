5450d109408f85a139f36966e9ee0493fdc94b6b
Chris Holston <chrisholston@me.com>
2020-11-10 23:58:25 -0600

    installs mongoose

--- config/database.js
+++ config/database.js
@@ -5,12 +5,12 @@ const mongoose = require ('mongoose');
 const db = mongoose.connection;
 
 // connect to the database
-mongoose.connect('mongodb+srv://admin:1234@cluster0.wyypc.mongodb.net/mongoose-flights?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://admin:1234@cluster0.wyypc.mongodb.net/mongoose-flights?retryWrites=true&w=majority',{
 useNewUrlParser: true,
 useCreateIndex: true,
 useUnifiedTopology: true
 });
 // listen for a connection event
 db.on('connected',function() {
-    console.log(`Connected to MongoDB on ${db.host}:${db.port}`)
+    console.log(`Connected to MongoDB on ${db.host}:${db.port}`);
 });
