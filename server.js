c87cf0d0e0d5a94c9acc8cd2fbb4ccdfebcc11ff
Chris Holston <chrisholston@me.com>
2020-11-16 14:53:38 -0600

    installed public folder

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
-// require our modules
+// require our modules -
+//1. create a variable called express and assign it to the result of requiring express.
+
+//ALSO MAKE SURE YOU INSTALL ALL DEPENDENCIES
 const express = require('express');
 const morgan = require ('morgan');
 const methodOverride = require('method-override');
 const mongoose = require ('mongoose');
 
-// create the express app
+// create the express app - we just call express like a function
 const app = express();
-// configure server settings
+
+// configure server settings- this is for our view engine. Make sure to install this as well.
 app.set('view engine', 'ejs');
 
 // mount middleware
